2f101a17e74b60c70f79ca52d30ccf984932000e
renatacarrillo <renata_carrillo02@hotmail.com>
2020-06-09 11:29:02 -0300

    Desenvolvimento pg Cadastro

--- cadastro.php
+++ cadastro.php
@@ -24,60 +24,61 @@
 </head>
 <body class='container-fluid'>
 <!-- INCLUINDO O HEADER -->
+<div class="row">
+    <div class="col mt-5 mb-5">
 <?php
 include ("includes/header.php");
 ?>
+</div>
+    </div>
 
 
 <section class="mt-5 mb-5">
-<div class="container-fluid">
-<div class="row">
-<div class="col-sm-12">
-<h1 class="text-center sombreamento-text"> EM CONSTRUÇÃO </h1>
-</div>
-</div>
-</div>
+    <div class="row">
+        <div class="col">
+            <h1 class="verdeesc text-center">FAÇA AGORA SEU CADASTRO</h1>
+        </div>
+    </div>
 </section>
 
-<section class="mt-5 mb-5">
-<div class="container-fluid">
-<div class="row">
-<div class="col-sm-12">
-<h1 class="text-center sombreamento-text"> EM CONSTRUÇÃO </h1>
-</div>
-</div>
-</div>
-</section>
 
-<section class="mt-5 mb-5">
-<div class="container-fluid">
+<div class="container mt-5 mb-5">
 <div class="row">
-<div class="col-sm-12">
-<h1 class="text-center sombreamento-text"> EM CONSTRUÇÃO </h1>
+<div class="col-md-6 offset-md-3">
+<form class="formulario">
+  <div class="verdecla form-group">
+    <label for="exampleInputEmail1">E-mail</label>
+    <input type="email" class="form-control" id="" placeholder="Digite seu email">
+  </div>
+  <div class="form-group verdecla">
+    <label for="exampleInputPassword1">Nome Completo</label>
+    <input type="name" class="form-control" id="" placeholder="Digite seu nome">
+  </div>
+  <div class="form-group verdecla">
+    <label for="exampleInputPassword1">Endereço</label>
+    <input type="name" class="form-control" id="" placeholder="Digite seu endereço">
+  </div>
+  <div class="form-group verdecla">
+    <label for="exampleInputPassword1">Telefone</label>
+    <input type="name" class="form-control" id="" placeholder="Digite seu telefone">
+  </div>
+  <div class="form-group verdecla">
+    <label for="exampleInputPassword1">CPF</label>
+    <input type="name" class="form-control" id="" placeholder="Digite seu cpf">
+  </div>
+  <div class="form-group verdecla">
+    <label for="exampleInputPassword1">RG</label>
+    <input type="name" class="form-control" id="" placeholder="Digite seu rg">
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label verdecla" for="exampleCheck1">Confirmar que está de acordo com a Política de Privacidade</label>
+  </div>
+  <button type="submit" class="btn btn-primary btn-lg btn-block">CADASTRAR</button>
+</form>
 </div>
 </div>
 </div>
-</section>
-
-<section class="mt-5 mb-5">
-<div class="container-fluid">
-<div class="row">
-<div class="col-sm-12">
-<h1 class="text-center sombreamento-text"> EM CONSTRUÇÃO </h1>
-</div>
-</div>
-</div>
-</section>
-
-<section class="mt-5 mb-5">
-<div class="container-fluid">
-<div class="row">
-<div class="col-sm-12">
-<h1 class="text-center sombreamento-text"> EM CONSTRUÇÃO </h1>
-</div>
-</div>
-</div>
-</section>
 
 <!-- INCLUINDO O FOOTER -->
         <!-- FOOTER -->
