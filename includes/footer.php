0a86a09893fc7b877b8ff059438a57ffc7ddbb0a
Reginaldo C Domingos <rcdomingos@live.com>
2020-09-08 22:53:04 -0300

    add solicitação e envio de cadastro

--- includes/footer.php
+++ includes/footer.php
@@ -1,52 +1,56 @@
-<?php
-echo
-"<footer class='page-footer pt-4'>
-    <!-- Footer Links -->
-<div class='container-fluid text-md-left'>
-        <div class='row'>
-            <!-- Grid column -->
-    <div class='col-sm mt-md-0 mt-3'>
-                <h4 class='text-uppercase'>BAIXE O APP AGORA:</h4>
-                <a href='https://play.google.com/store?hl=pt_BR' class='text-white'>PlayStore</a>
-                <div class='col-sm'>
-                    <a href='https://www.apple.com/br/ios/app-store/' class='text-white'>AppStore</a>
-                </div>
-            </div>
-            <!-- Grid column -->
-        <div class='col-sm mb-md-0 mb-3 mg-mob-foot'>
-                <!-- Links -->
+<footer class='page-footer pt-4'>
+  <!-- Footer Links -->
+  <div class='container-fluid text-md-left'>
+    <div class='row'>
+      <!-- Grid column -->
+      <div class='col-sm mt-md-0 mt-3'>
+        <h4 class='text-uppercase'>BAIXE O APP AGORA:</h4>
+        <a href='https://play.google.com/store?hl=pt_BR' class='text-white'>PlayStore</a>
+        <div class='col-sm'>
+          <a href='https://www.apple.com/br/ios/app-store/' class='text-white'>AppStore</a>
+        </div>
+      </div>
+      <!-- Grid column -->
+      <div class='col-sm mb-md-0 mb-3 mg-mob-foot'>
+        <!-- Links -->
         <h4 class=''>INFORMATIVOS</h4>
-                <ul class='list-unstyled'>
-                    <li>
-                        <a href='quemsomos.php' class='text-white'>Quem Somos</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- Grid column -->
-    <div class='col-sm-3 mb-md-0 mb-3'>
-                <!-- Links -->
-    <h4 class=''>SITE</h4>
-                <ul class='list-unstyled'>
-                    <li>
-                        <a href='cadastro.php' class='text-white'>Home</a>
-                    </li>
-                    <li>
-                        <a href='noticias.php' class='text-white'>Notícias</a>
-                    </li>
-                    <li>
-                        <a href='projeto.php' class='text-white'>Projeto</a>
-                    </li>
-                    <li>
-                        <a href='cadastro.php' class='text-white'>Cadastre-se</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- Grid column -->
-</div>
-        <!-- Grid row -->
-</div>
-    <!-- Copyright -->
-<div class='text-center mb-1 mt-5'>
-        <img src='./IMAGENS/CATATUDO.png' class='img-foot-tam img-fluid' alt='logotipo CATATUDO'>
-</div>
-</footer>";
+        <ul class='list-unstyled'>
+          <li>
+            <a href='quemsomos.php' class='text-white'>Quem Somos</a>
+          </li>
+        </ul>
+      </div>
+      <!-- Grid column -->
+      <div class='col-sm-3 mb-md-0 mb-3'>
+        <!-- Links -->
+        <h4 class=''>SITE</h4>
+        <ul class='list-unstyled'>
+          <li>
+            <a href='cadastro.php' class='text-white'>Home</a>
+          </li>
+          <li>
+            <a href='noticias.php' class='text-white'>Notícias</a>
+          </li>
+          <li>
+            <a href='projeto.php' class='text-white'>Projeto</a>
+          </li>
+          <li>
+            <a href='cadastro.php' class='text-white'>Cadastre-se</a>
+          </li>
+        </ul>
+      </div>
+      <!-- Grid column -->
+    </div>
+    <!-- Grid row -->
+  </div>
+  <!-- Copyright -->
+  <div class='text-center mb-1 mt-5'>
+    <img src='./IMAGENS/CATATUDO.png' class='img-foot-tam img-fluid' alt='logotipo CATATUDO'>
+  </div>
+</footer>
+
+<!-- scripts js  -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="<?php echo SITE_URL ?>/js/jquery-3.5.1.min.js"></script>
+<script src="<?php echo SITE_URL ?>/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo SITE_URL ?>/js/registrer.js"></script>
---
+++ includes/formCadastro.php
@@ -0,0 +1,39 @@
+<section class="side-registrer">
+  <a tabindex="-1" href="#" class="slide-register-close text-right">X</a>
+  <div id="section-register">
+    <h3>Quero reciclar</h3>
+    <p><small>Preencha seus dados para começar a reciclar</small></p>
+    <div id="feedResponse"></div>
+    <form name="form-cadastro" class=" form-cadastro" method="POST" oninput='passwordRPT.setCustomValidity(passwordRPT.value != password.value ? "As senhas não conferem." : "")'>
+      <div class="form-group verdecla">
+        <label for="name">Nome Completo</label>
+        <input type="text" class="form-control" name="name" placeholder="Digite seu nome" required>
+      </div>
+      <div class="verdecla form-group">
+        <label for="email">E-mail</label>
+        <input type="email" class="form-control" name="email" placeholder="Digite seu email" required>
+      </div>
+      <div class="form-group verdecla">
+        <label for="phone">Telefone</label>
+        <input type="text" class="form-control" name="phone" placeholder="Digite seu telefone">
+      </div>
+      <div class="form-group verdecla">
+        <label for="password">Senha</label>
+        <input type="password" class="form-control" name="password" placeholder="Digite sua senha" required>
+      </div>
+      <div class="form-group verdecla">
+        <label for="passwordRPT">Confirme a Senha</label>
+        <input type="password" class="form-control" name="passwordRPT" placeholder="Repita sua senha" required>
+      </div>
+      <div class="custom-control custom-checkbox mb-3">
+        <input type="checkbox" class="custom-control-input" id="isOk" name="isOk" required>
+        <label class="custom-control-label verdecla" for="isOk">Confirmar que está de acordo com a Política de
+          Privacidade</label>
+        <div class="invalid-feedback">Confirme que está de acordo com a Política de Privacidade</div>
+      </div>
+      <button type="submit" class="btn btn-success btn-lg btn-block">CADASTRAR</button>
+    </form>
+
+  </div>
+</section>
+<div class="side-section-overlay" style="width: 0px; opacity: 0;"></div>
